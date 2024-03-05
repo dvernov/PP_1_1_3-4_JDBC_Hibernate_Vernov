0482fbc6cd13e9bf656651d36fb195482a6aed36
archimg <danqajkee@gmail.com>
2024-03-05 18:43:58 +0300

    fixed main according to task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -21,23 +21,12 @@ public class Main {
         userDao.saveUser("Vasya1", "Petrov1", (byte) 1);
         userDao.saveUser("Vasya2", "Petrov2", (byte) 2);
         userDao.saveUser("Vasya3", "Petrov3", (byte) 3);
-        userDao.saveUser("Vasya3", "Petrov3", (byte) 3); // testing
         userDao.saveUser("Vasya4", "Petrov4", (byte) 4);
-        //testing hql
-        try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
 
-            session.createQuery("from User where name like '%sya%' and age between 2 and 4")
-                    .getResultList()
-                    .forEach(System.out::println);
-            session.getTransaction().commit();
+        List<User> usersList = userDao.getAllUsers();
+        for (User user : usersList) {
+            System.out.println(user.toString());
         }
-        //testing hql
-
-//        List<User> usersList = userDao.getAllUsers();
-//        for (User user : usersList) {
-//            System.out.println(user.toString());
-//        }
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
     }
